9617f2c24b4a46284c0567741d16b750542abf91
Sventsytskyi Tymofii <tim.from.ua@gmail.com>
2024-10-13 09:02:45 +0300

    Not important

--- src/HandlerFactory.java
+++ src/HandlerFactory.java
@@ -1,8 +1,11 @@
 public class HandlerFactory
 {
-    public static FileHandler create_handler(String type)
+    public static FileHandler create_handler(String ext)
     {
-        return switch(type)
+        if (!ext.contains("."))
+            ext = "." + ext;
+
+        return switch(ext)
         {
             case ".pdf" -> new PDFHandler();
             case ".txt" -> new TXTHandler();
